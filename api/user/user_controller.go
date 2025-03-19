58a956554eb1e2402cb3099078a662fb932955cb
rolando-d3v <landon3388@gmail.com>
2025-03-19 08:19:49 -0500

    Update changes

--- api/user/user_controller.go
+++ api/user/user_controller.go
@@ -9,7 +9,7 @@ func GetIdUser(w http.ResponseWriter, r *http.Request) {
 
     // fmt.Printf("Heyyy Am getting called From Controllers")
 
-    w.Write([]byte(`{"message": "working one user 2022..."}`))
+    w.Write([]byte(`{"msg": "React one user 2022..."}`))
 
 }
 func GetAllUser(w http.ResponseWriter, r *http.Request) {
