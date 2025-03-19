fafdd92c9758def0dfd4f8bff35a6e97cc338e7a
rolando-d3v <landon3388@gmail.com>
2025-03-19 18:02:30 -0500

    Update changes

--- api/user/user_controller.go
+++ api/user/user_controller.go
@@ -3,6 +3,8 @@ package user
 import (
     // "fmt"
     "net/http"
+    "fmt"
+	"time"
 )
 
 func GetIdUser(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,25 @@ func GetIdUser(w http.ResponseWriter, r *http.Request) {
 func GetAllUser(w http.ResponseWriter, r *http.Request) {
 
     // fmt.Printf("Heyyy Am getting called From Controllers")
+
+    	// Iniciar el cronómetro
+	start := time.Now()
+
+	// Crear un slice con 9,000,000 elementos
+	registros := make([]int, 90000000)
+	for i := 0; i < 90000000; i++ {
+		registros[i] = i + 1
+	}
+
+	// Leer los registros con un bucle `for`
+	for i := 0; i < len(registros); i++ {
+		dato := registros[i] // Simulación de lectura
+		_ = dato             // Evitar warnings de variable no utilizada
+	}
+
+	// Calcular el tiempo total transcurrido
+	elapsed := time.Since(start)
+	fmt.Printf("Tiempo total: %s\n", elapsed)
 
     w.Write([]byte(`{"message": "working users...", "pepe": 15}`))
 
